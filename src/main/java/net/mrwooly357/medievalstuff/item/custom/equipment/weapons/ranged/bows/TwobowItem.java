5232a79d97fcad05f8069dd9065c93c2c5174efe
MrWooly125 <belov.20.04@mail.ru>
2025-06-06 19:47:02 +0300

    Some updates and stuff

--- src/main/java/net/mrwooly357/medievalstuff/item/custom/equipment/weapons/ranged/bows/TwobowItem.java
+++ src/main/java/net/mrwooly357/medievalstuff/item/custom/equipment/weapons/ranged/bows/TwobowItem.java
@@ -8,18 +8,43 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.RangedWeaponItem;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.function.Predicate;
 
-import static net.mrwooly357.medievalstuff.config.ModConfigValues.Items.Weapons.Ranged.Bows.Twobow.*;
-
 public class TwobowItem extends RangedWeaponItem {
+
+    public static float PROJECTILE_SPEED_MULTIPLIER = 3.5F;
+    public static float MIN_PROJECTILE_DIVERGENCE = 0.75F;
+    public static float MAX_PROJECTILE_DIVERGENCE = 1.0F;
+    public static float PROJECTILE_DIVERGENCE = MathHelper.nextFloat(Random.create(), MIN_PROJECTILE_DIVERGENCE, MAX_PROJECTILE_DIVERGENCE);
+    public static float ADDITIONAL_PROJECTILE_SPEED_MULTIPLIER = 3.5F;
+    public static float MIN_ADDITIONAL_PROJECTILE_DIVERGENCE = 0.5F;
+    public static float MAX_ADDITIONAL_PROJECTILE_DIVERGENCE = 0.75F;
+    public static float ADDITIONAL_PROJECTILE_DIVERGENCE = MathHelper.nextFloat(Random.create(), MIN_ADDITIONAL_PROJECTILE_DIVERGENCE, MAX_ADDITIONAL_PROJECTILE_DIVERGENCE);
+    public static float MIN_ADDITIONAL_PROJECTILE_ADDITIONAL_PITCH = 2.0F;
+    public static float MAX_ADDITIONAL_PROJECTILE_ADDITIONAL_PITCH = 2.5F;
+    public static float ADDITIONAL_PROJECTILE_ADDITIONAL_PITCH = MathHelper.nextFloat(Random.create(), MIN_ADDITIONAL_PROJECTILE_ADDITIONAL_PITCH, MAX_ADDITIONAL_PROJECTILE_ADDITIONAL_PITCH);
+    public static SoundEvent USE_SOUND = SoundEvents.ENTITY_ARROW_SHOOT;
+    public static float MIN_USE_SOUND_VOLUME = 0.8F;
+    public static float MAX_USE_SOUND_VOLUME = 1.2F;
+    public static float TWOBOW_USE_SOUND_VOLUME = MathHelper.nextFloat(Random.create(), MIN_USE_SOUND_VOLUME, MAX_USE_SOUND_VOLUME);
+    public static float MIN_USE_SOUND_PITCH = 0.8F;
+    public static float MAX_USE_SOUND_PITCH = 1.2F;
+    public static float USE_SOUND_PITCH = MathHelper.nextFloat(Random.create(), MIN_USE_SOUND_PITCH, MAX_USE_SOUND_PITCH);
+    public static UseAction USE_ANIMATION = UseAction.BOW;
+    public static int MAX_USE_TIME = 72000;
+    public static int TICKS_PER_SECOND = 20;
+    public static int RANGE = 15;
 
     public TwobowItem(Settings settings) {
         super(settings);
